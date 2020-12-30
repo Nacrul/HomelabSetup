20409105fee445bd9ec0de76e7520164892c6903
Nacrul <micklenk@gmail.com>
2020-12-30 13:04:29 +0100

    Add demo variables file before adding to ignore list

---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+# demo variables file, needs to be changed locally!
+variable "pm_api_url" {
+  default = "https://"ip of your proxmox server":8006/api2/json"
+}
+
+variable "pm_user" {
+default = "root@pam"
+}
+
+variable "pm_password" {
+default = "3277db3c"
+}
